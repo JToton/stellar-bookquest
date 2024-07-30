23724b9bb247184605fc029fc9141257d949e64f
JacobToton <Jake_Toton@live.com>
2024-07-30 07:11:22 -0600

    Overnight work, Updating and refactoring to use mutations, remove old components, and refactor code - needs rigorous testing
    - Imported necessary Apollo Client components and functions
    - Created an HTTP link that points to GraphQL endpoint
    - Created an auth link that attaches the JWT token to the authorization header of each request
    - Set up the Apollo Client instance with the configured link and cache
    - Wrapped the entire app with the ApolloProvider component, passing in the client instance GraphQL mutations for login and signup
    - Imported useMutation from @apollo/client and the LOGIN_USER mutation from ../utils/mutations
    - Removed the import of loginUser from ../utils/API as we're no longer using the REST API
    - Added the useMutation hook to use the LOGIN_USER mutation
    - Updated the handleFormSubmit function to use the login mutation instead of the loginUser API call
    - Removed the username field from the setUserFormData call at the end of handleFormSubmit as it's not used in the login form
    - Created queries.js file for API calls
    - Imported useMutation from @apollo/client and the ADD_USER mutation from ../utils/mutations
    - Removed the import of createUser from ../utils/API as we're no longer using the REST API
    - Added the useMutation hook to use the ADD_USER mutation
    - Updated the handleFormSubmit function to use the addUser mutation instead of the createUser API call
    - Imported useQuery and useMutation from @apollo/client, and imported GET_ME query and REMOVE_BOOK mutation
    - Removed useState and useEffect hooks as we're now using Apollo's useQuery hook
    - Replaced the getMe API call with the useQuery hook
    - Added the useMutation hook for the REMOVE_BOOK mutation
    - Updated handleDeleteBook to use the removeBook mutation instead of the deleteBook API call
    - Updated the component to use loading from useQuery instead of checking userDataLength
    - Changed how we access user data: const userData = data?.me || {};
    - Updated the JSX to safely access userData.savedBooks using optional chaining
    - Moved the key prop from Card to Col component for better React performance
    - Imported useMutation from @apollo/client and the SAVE_BOOK mutation from ../utils/mutations
    - Removed the import of saveBook from ../utils/API as we're now using the GraphQL mutation
    - Added the useMutation hook to use the SAVE_BOOK mutation
    - Updated the handleSaveBook function to use the saveBook mutation instead of the saveBook API call
    - Removed the token passing to the saveBook function as it will be handled by the Apollo Client setup
    - Created mutations.js mutations
    - LOGIN_USER, ADD_USER, SAVE_BOOK, REMOVE_BOOK
    
    - Needs Debugging:
    - ADD_USER (account creation)
    - Login feature (LOGIN_USER)
    
    - Needs Testing:
    - Once Account creation is functioning -> Test SAVE_BOOK and REMOVE_BOOK

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require("apollo-server-express");
+const { GraphQLError } = require("graphql");
 const { User } = require("../models");
 const { signToken } = require("../utils/auth");
 
@@ -8,7 +8,9 @@ const resolvers = {
       if (context.user) {
         return User.findOne({ _id: context.user._id }).populate("savedBooks");
       }
-      throw new AuthenticationError("You need to be logged in!");
+      throw new GraphQLError("You need to be logged in!", {
+        extensions: { code: "UNAUTHENTICATED" },
+      });
     },
   },
 
@@ -22,13 +24,17 @@ const resolvers = {
       const user = await User.findOne({ email });
 
       if (!user) {
-        throw new AuthenticationError("No user found with this email address");
+        throw new GraphQLError("No user found with this email address", {
+          extensions: { code: "UNAUTHENTICATED" },
+        });
       }
 
       const correctPw = await user.isCorrectPassword(password);
 
       if (!correctPw) {
-        throw new AuthenticationError("Incorrect credentials");
+        throw new GraphQLError("Incorrect credentials", {
+          extensions: { code: "UNAUTHENTICATED" },
+        });
       }
 
       const token = signToken(user);
@@ -44,7 +50,9 @@ const resolvers = {
         );
         return updatedUser;
       }
-      throw new AuthenticationError("You need to be logged in!");
+      throw new GraphQLError("You need to be logged in!", {
+        extensions: { code: "UNAUTHENTICATED" },
+      });
     },
     removeBook: async (parent, { bookId }, context) => {
       if (context.user) {
@@ -55,7 +63,9 @@ const resolvers = {
         );
         return updatedUser;
       }
-      throw new AuthenticationError("You need to be logged in!");
+      throw new GraphQLError("You need to be logged in!", {
+        extensions: { code: "UNAUTHENTICATED" },
+      });
     },
   },
 };
